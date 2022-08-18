340cadd173a87881743a8912892f7a8bec4e3172
Daniela Lavric <daniela@hashicorp.com>
2022-08-18 12:13:45 +0200

    trying to trigger a run
    triggering a run

--- root-directory/main.tf
+++ root-directory/main.tf
@@ -2,5 +2,5 @@ resource "null_resource" "null" {
 }
 
 output "myoutput" {
-  value = "This run has been triggered from my root-directory Daniela triggers another run alabalal"
+  value = "This run has been triggered from my root-directory Daniela blabla"
 }
