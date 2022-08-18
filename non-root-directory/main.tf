a12f40038799ac91db08c8f597d9441fd4487263
Daniela Lavric <daniela@hashicorp.com>
2022-08-18 11:33:42 +0200

    Update main.tf

--- non-root-directory/main.tf
+++ non-root-directory/main.tf
@@ -2,5 +2,5 @@ resource "null_resource" "null" {
 }
 
 output "myoutput" {
-  value = "This run has been triggered from a non-root-directory"
+  value = "This run has been triggered from a non-root-directory blabla"
 }
