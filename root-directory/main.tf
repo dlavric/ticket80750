0589f51b88b4c846e90a7fe554e281a436899902
Daniela Lavric <daniela@hashicorp.com>
2022-08-18 12:08:33 +0200

    Update main.tf

--- root-directory/main.tf
+++ root-directory/main.tf
@@ -2,5 +2,5 @@ resource "null_resource" "null" {
 }
 
 output "myoutput" {
-  value = "This run has been triggered from my root-directory checking if this triggers a run"
+  value = "This run has been triggered from my root-directory Daniela triggers a run"
 }
