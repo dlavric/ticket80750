7d1576c11d5d1e734bb051402e97ebe5617a7336
Daniela Lavric <daniela@hashicorp.com>
2022-09-02 16:14:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "null_resource" "null" {
 }
 
 output "myoutput" {
-  value = "This run has been triggered from my repository try 1"
+  value = "This run has been triggered from my repository"
 }
